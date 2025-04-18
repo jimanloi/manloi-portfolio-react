70953a18ecaa580f6ec62c4d2d6412f65d04cb5d
jimanloi <jimanloi@hotmail.com>
2025-04-18 17:37:21 +0200

    updated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,33 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footer-section">
+      <div className="social-media">
+        <a href="https://github.com/jimanloi">
+          <i
+            class="bi bi-github"
+            style={{ color: "black", fontSize: "20px" }}
+          ></i>
+        </a>
+        <a href="https://www.linkedin.com/in/manloij/">
+          <i
+            class="bi bi-linkedin"
+            style={{ color: "black", fontSize: "20px" }}
+          ></i>
+        </a>
+        <a href="https://www.instagram.com/jmseastheday/">
+          <i
+            class="bi bi-instagram"
+            style={{ color: "black", fontSize: "20px" }}
+          ></i>
+        </a>
+      </div>
+      <div className="copyright">
+        <small>© designed & built by Manloi Jim with React</small>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
